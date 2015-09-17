40477bc5e9f80b3188de9fbf497245fa8ca01f4f
wbond <will@wbond.net>
2015-09-17 00:25:12 -0400

    Version 1.0.5

--- package_coverage.py
+++ package_coverage.py
@@ -30,8 +30,8 @@ else:
     from cStringIO import StringIO
 
 
-__version__ = '1.0.4'
-__version_info__ = (1, 0, 4)
+__version__ = '1.0.5'
+__version_info__ = (1, 0, 5)
 
 
 class PackageCoverageExecCommand(sublime_plugin.WindowCommand):
